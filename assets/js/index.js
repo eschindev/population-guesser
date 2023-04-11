4a0f2cc388b18256d7a2d69b92f6b0d30499cf3b
David C <dchung20315@gmail.com>
2023-04-11 11:24:26 -0700

    added map tile, and pin to the map

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,6 +19,15 @@ fetch(`https://wft-geo-db.p.rapidapi.com/v1/geo/cities?limit=1&offset=${randomCi
         console.log(cityName);
 		cityLat = response.data[0].latitude;
 		cityLon = response.data[0].longitude;
+
+		var map = L.map('map').setView([cityLat, cityLon], 15);
+		L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+          attribution: 'Data <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, '
+          + 'Map tiles &copy; <a href="https://carto.com/attribution">CARTO</a>'
+        }).addTo(map);
+		var marker = L.marker([cityLat, cityLon]);
+		marker.addTo(map);
+
 		var OpenCageApi = "d05c7ded07e944bca02c922313d20342";
 		var mapRequestUrl = `https://api.opencagedata.com/geocode/v1/json?q=${cityLat}+${cityLon}&key=${OpenCageApi}&pretty=1`;
 		fetch(mapRequestUrl)
