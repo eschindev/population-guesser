1c233a045e5da6f09326d6c416d163c6e538ee44
eschindev <118208710+eschindev@users.noreply.github.com>
2023-04-11 14:21:33 -0700

    moved functions into game object, added submit event listener, calculate score in checkGuess

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,23 @@
 var cityName;
-var cityNameHtml = $("<h3>");
-var cityNameArea = $('#city-name');
 var cityLat;
 var cityLon;
+var cityPop;
+var playerGuess;
+var score;
 // var OpenCageApi = "d05c7ded07e944bca02c922313d20342";
+
+var cityNameHtml = $('<h3>');
+var cityNameArea = $('#city-name');
+var playerInput = $('#player-input');
+
+playerInput.on('submit', submitGuess);
+
+var submitGuess = function(event) {
+	event.preventDefault();
+
+	playerGuess = $('#user-guess').val();
+	game.checkGuess(playerGuess);
+}
 
 
 var randomCityNum = Math.floor(Math.random() * 602086);
@@ -16,18 +30,21 @@ const options = {
 	}
 };
 
-
-
-fetch(`https://wft-geo-db.p.rapidapi.com/v1/geo/cities?limit=1&offset=${randomCityNum}`, options)
+// object to contain all functions related to the game
+var game = {
+    startGame: function() {
+        fetch(`https://wft-geo-db.p.rapidapi.com/v1/geo/cities?limit=1&offset=${randomCityNum}`, options)
 	.then(response => response.json())
 	.then(response => {
         console.log(response);
+
         cityName = response.data[0].city;
+        cityLat = response.data[0].latitude;
+		cityLon = response.data[0].longitude;
+        cityPop = response.data[0].population;
         cityNameHtml.text(cityName);
         cityNameArea.append(cityNameHtml);
-        console.log(cityName);
-		cityLat = response.data[0].latitude;
-		cityLon = response.data[0].longitude;
+
 
 		var map = L.map('map').setView([cityLat, cityLon], 15);
 		L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
@@ -45,6 +62,55 @@ fetch(`https://wft-geo-db.p.rapidapi.com/v1/geo/cities?limit=1&offset=${randomCi
 
     })
 	.catch(err => console.error(err));
+    },
+	checkGuess: function(guess) {
+		if (cityPop / guess < 1) {
+			score = Math.round((1 - (cityPop / guess)) * 100);
+		} else {
+			score = Math.round((1 - (guess / cityPop)) * 100);
+		}
+	}
+}
+
+
+game.startGame();
+
+
+
+
+
+
+
+
+
+
+// fetch(`https://wft-geo-db.p.rapidapi.com/v1/geo/cities?limit=1&offset=${randomCityNum}`, options)
+// 	.then(response => response.json())
+// 	.then(response => {
+//         console.log(response);
+//         cityName = response.data[0].city;
+//         cityNameHtml.text(cityName);
+//         cityNameArea.append(cityNameHtml);
+//         console.log(cityName);
+// 		cityLat = response.data[0].latitude;
+// 		cityLon = response.data[0].longitude;
+
+// 		var map = L.map('map').setView([cityLat, cityLon], 15);
+// 		L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+//           attribution: 'Data <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, '
+//           + 'Map tiles &copy; <a href="https://carto.com/attribution">CARTO</a>'
+//         }).addTo(map);
+// 		var marker = L.marker([cityLat, cityLon]);
+// 		marker.addTo(map);
+
+// 		// var mapRequestUrl = `https://api.opencagedata.com/geocode/v1/json?q=${cityLat}+${cityLon}&key=${OpenCageApi}&pretty=1`;
+// 		// fetch(mapRequestUrl)
+// 		// 	.then(response=>response.json())
+// 		// 	.then(response => {
+//         // 		console.log(response);})
+
+//     })
+// 	.catch(err => console.error(err));
 
 	
 	
